68bf79e3186e4a3304fa07005178edb69116cabd
Temirkhan <temirkhan@nasukhov.me>
2024-08-13 22:39:19 +0200

    A bit more interactive version

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var chars = characters.entries.where((elem) => elem.value is! LinkingCharacter).toList();
     LinkingCharacter supportCharacter = characters[CharacterName.lunaire] as LinkingCharacter;
-    Character mainCharacter = characters[CharacterName.saras]!;
 
     return MaterialApp(
       title: 'KGC Calculator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        //useMaterial3: true,
+      home: Scaffold(
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4// Spacing between rows
+          ),
+          itemCount: chars.length,
+          itemBuilder: (BuildContext context, int index) {
+            var character = chars[index].value;
+
+            return ElevatedButton(
+                onPressed: () => openPage(LinkStatBuffCalculator(character, supportCharacter), context),
+                child: Text(character.name));
+          },
+        ),
       ),
-      home: LinkStatBuffCalculator(mainCharacter, supportCharacter),
     );
   }
+}
+
+void openPage(StatelessWidget page, BuildContext withContext) {
+  Navigator.push(withContext, MaterialPageRoute(builder: (context) => page));
 }
--- lib/widget/linkbuff_stat_calculator.dart
+++ lib/widget/linkbuff_stat_calculator.dart
@@ -1,18 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:god_king_castle_calculator/character.dart';
 import 'package:god_king_castle_calculator/linked_stats_matrix.dart';
 import 'package:god_king_castle_calculator/widget/character_stats.dart';
 
 class LinkStatBuffCalculator extends StatelessWidget {
-  final Character main;
-  final LinkingCharacter support;
+  final Character _main;
+  final LinkingCharacter _support;
 
-  const LinkStatBuffCalculator(this.main, this.support, {super.key});
+  const LinkStatBuffCalculator(this._main, this._support, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    StatsMatrix statsMatrix = StatsMatrix(main, support);
+    StatsMatrix statsMatrix = StatsMatrix(_main, _support);
 
     return Scaffold(
       appBar: AppBar(
